436286d885f4e911253bcf254bf5fef1e4c23be8
jguedez92 <jguedez92@gmail.com>
2020-07-06 00:34:08 +0200

    change url of images on api-config

--- src/api-config.js
+++ src/api-config.js
@@ -1,9 +1,15 @@
 let backendHost;
+let urlImg;
 if (window.location.href.includes('localhost')) {
     backendHost = 'http://localhost:8000/api'
 } else {
     backendHost = 'https://laravel-e-commerce-api.herokuapp.com/api'
 }
+if (window.location.href.includes('localhost')) {
+    urlImg = 'http://localhost:8000/images/'
+} else {
+    urlImg = 'https://laravel-e-commerce-api.herokuapp.com/images/'
+}
 console.log(backendHost)
 export const API_URL = `${backendHost}/`;
-export const IMAGES_URL = `http://localhost:8000/images/`;
+export const IMAGES_URL = `${urlImg}`;
